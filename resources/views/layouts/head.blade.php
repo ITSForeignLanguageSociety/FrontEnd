5f25e729f601c7546d1a3c435feec616e159562e
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2021-07-23 01:02:28 +0700

    update header

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,23 +1,23 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    {{-- <link rel="icon" href="/assets/img/logo/logo.png"> --}}
+    <link rel="icon" href="/assets/img/logo/logo.png">
     <title>Beranda - ITS Foreign Language Society</title>
 
     {{-- <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}"> --}}
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/Simple-Slider.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/smoothproducts.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/fontawesome5-overrides.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css', true) }}">
+    <link rel="stylesheet" href="{{ asset('css/Simple-Slider.css', true) }}">
+    <link rel="stylesheet" href="{{ asset('css/smoothproducts.css', true) }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css', true) }}">
+    <link rel="stylesheet" href="{{ asset('css/fontawesome5-overrides.min.css', true) }}">
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css">
+    <link rel="preconnect" href="//fonts.googleapis.com">
+    <link rel="preconnect" href="//fonts.gstatic.com" crossorigin>
+    <link href="//fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.12.0/css/all.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css">
 
     <style>
         body {
