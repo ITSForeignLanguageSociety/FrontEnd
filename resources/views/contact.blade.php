7bc3e3f7bfc63246103eea5ac0c9d9b971d7066d
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2021-08-07 22:39:23 +0700

    edit contact view

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -9,7 +9,11 @@
         <div class="col-lg-4 mb-3 d-flex justify-content-center align-items-center">
             <div class="nilai-card" style="width: 18rem;">
                 <div class="card-body text-center">
-                    <h1 class="card-title mb-1"><i class="fab fa-line mr-1"></i></h1>
+                    <h1 class="card-title mb-1">
+                        <a href="" target="_blank" style="color: #00a500!important">
+                            <i class="fab fa-line mr-1"></i>
+                        </a>
+                    </h1>
                     <p class="card-text">
                         <strong>@atc9575r</strong>
                     </p>
@@ -19,7 +23,17 @@
         <div class="col-lg-4 mb-3 d-flex justify-content-center align-items-center">
             <div class="nilai-card" style="width: 18rem;">
                 <div class="card-body text-center">
-                    <h1 class="card-title mb-1"><i class="fa fa-facebook mr-3"></i><i class="fa fa-youtube-play mr-3"></i><i class="fa fa-spotify"></i></h1>
+                    <h1 class="card-title mb-1">
+                        <a href="https://www.facebook.com/IFLS.ITS/" target="_blank" style="color: blue!important; text-decoration: none!important">
+                            <i class="fa fa-facebook mr-3"></i>
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCZQwvpzZhZ7n54D1PtGNcCg" target="_blank" style="color: red!important; text-decoration: none!important">
+                            <i class="fa fa-youtube-play mr-3"></i>
+                        </a>
+                        <a href="https://open.spotify.com/episode/4Wx85CRN3J3xiz8cJXZYD8" target="_blank" style="color: #00a000!important; text-decoration: none!important">
+                            <i class="fa fa-spotify"></i>
+                        </a>
+                    </h1>
                     <p class="card-text">
                         <strong>ITS Foreign Language Society</strong>
                     </p>
@@ -29,7 +43,7 @@
         <div class="col-lg-4 mb-3 d-flex justify-content-center align-items-center">
             <div class="nilai-card" style="width: 18rem;">
                 <div class="card-body text-center">
-                    <h1 class="card-title mb-1"><i class="fa fa-instagram mr-1"></i></h1>
+                    <h1 class="card-title mb-1"><a href="https://www.instagram.com/ifls_its/" target="_blank" style="color: pink!important; text-decoration: none!important"><i class="fa fa-instagram mr-1"></a></i></h1>
                     <p class="card-text">
                         <strong>@ifls_its</strong>
                     </p>
