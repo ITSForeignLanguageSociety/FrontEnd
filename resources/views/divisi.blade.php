1d578fea2f5879fc6729a511934893f7752ac3ec
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2021-08-25 16:45:36 +0700

    update divisi view

--- resources/views/divisi.blade.php
+++ resources/views/divisi.blade.php
@@ -1,5 +1,72 @@
 @extends('layouts.base')
 
 @section('content')
-<h1>divisi</h1>
+<section class="clean-block section-light-blue" style="min-height: 900px; background: center center fixed; background-image:url('img/divisi/divisi-banner.png');color:rgba(0, 0, 0, 0); margin-top:17px;">
+    <div class="container" style="padding-top: 12em">
+        <div class="row d-flex justify-content-center align-items-center">
+            <div class="col-lg-4 mb-3 d-flex justify-content-center align-items-center">
+                <a href="" target="_blank" style="text-decoration: none!important">
+                    <div class="divisi-card">
+                        <div class="card-body text-center">
+                            <h3 class="card-title mb-4"><strong>IDS</strong></h3>
+                            <p class="card-text">
+                                <strong>IDS adalah singkatan dari ITS Debating Society</strong>
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 mb-3 d-flex justify-content-center align-items-center">
+                <a href="" target="_blank" style="text-decoration: none!important">
+                    <div class="divisi-card">
+                        <div class="card-body text-center">
+                            <h3 class="card-title mb-4"><strong>IJCA</strong></h3>
+                            <p class="card-text">
+                                <strong>IJCA atau IFLS Japanese Cultural Association adalah divisi jepang ITS Foreign Language Society</strong>
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 mb-3 d-flex justify-content-center align-items-center">
+                <a href="" target="_blank" style="text-decoration: none!important">
+                    <div class="divisi-card">
+                        <div class="card-body text-center">
+                            <h3 class="card-title mb-4"><strong>IHMHA</strong></h3>
+                            <p class="card-text">
+                                <strong>IHMHA atau IFLS Hamunhwaae merupakan divisi Korea dari IFLS</strong>
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="row d-flex justify-content-center align-items-center">
+            <div class="col-lg-4 mb-3 d-flex justify-content-center align-items-center">
+                <a href="" target="_blank" style="text-decoration: none!important">
+                    <div class="divisi-card">
+                        <div class="card-body text-center">
+                            <h3 class="card-title mb-4"><strong>DC</strong></h3>
+                            <p class="card-text">
+                                <strong>DC adalah singkatan dari Dueling Cards</strong>
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 mb-3 d-flex justify-content-center align-items-center">
+                <a href="" target="_blank" style="text-decoration: none!important">
+                    <div class="divisi-card">
+                        <div class="card-body text-center">
+                            <h3 class="card-title mb-4"><strong>IC</strong></h3>
+                            <p class="card-text">
+                                <strong>IFLS Culture atau IC adalah divisi IFLS yang berfokus pada budaya.</strong>
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
