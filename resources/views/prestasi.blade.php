25855452cd4957f4c132a31aedd3fd546da43d6c
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2021-08-29 13:16:51 +0700

    finish Prestasi View

--- resources/views/prestasi.blade.php
+++ resources/views/prestasi.blade.php
@@ -10,7 +10,7 @@
         </div>
     </section>
     <section class="section clean-block container pt-5">
-        <div class="row d-flex justify-content-center align-items-center">
+        <div class="large-prestasi row d-flex justify-content-center align-items-center mb-4">
             <div class="col-2">
                 <div class="row d-flex justify-content-end">
                     <p><strong>Omahira</strong></p>
@@ -35,6 +35,163 @@
                 </div>
                 <div class="row">
                     <p><strong>Putu Krisna</strong></p>
+                </div>
+            </div>
+        </div>
+        <div class="small-prestasi row pb-5">
+            <div class="col-12">
+                <div class="row mb-5">
+                    <div class="col-12 text-center">
+                        <h4><strong>Quarterfinalist Asian English Olympiad</strong></h4>
+                        <h4 class="prestasi-divisi"><strong>ITS Debating Society</strong></h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 d-flex justify-content-center">
+                        <div class="row">
+                            <img class="prestasi-card py-3" src="{{ asset('img/prestasi/oma.png') }}" alt="">
+                        </div>
+                    </div>
+                    <div class="col-6 d-flex justify-content-center">
+                        <div class="row ">
+                            <img class="prestasi-card py-3" src="{{ asset('img/prestasi/krisna.png') }}" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="large-prestasi row d-flex justify-content-center align-items-center mb-4">
+            <div class="col-2">
+                <div class="row d-flex justify-content-end">
+                    <p><strong>Omahira</strong></p>
+                </div>
+                <div class="row">
+                    <img class="prestasi-card" src="{{ asset('img/prestasi/oma.png') }}" alt="">
+                </div>
+                <div class="row">
+                    <img class="prestasi-circle" src="{{ asset('img/prestasi/circle.png') }}" alt="">
+                </div>
+            </div>
+            <div class="col-8" style="text-align: center">
+                <h4><strong>Co-2<sup>nd</sup> Runner Up DUNNO 2021</strong></h4>
+                <h4 class="prestasi-divisi"><strong>ITS Debating Society</strong></h4>
+            </div>
+            <div class="col-2">
+                <div class="row">
+                    <img class="prestasi-circle" src="{{ asset('img/prestasi/circle.png') }}" alt="">
+                </div>
+                <div class="row">
+                    <img class="prestasi-card" src="{{ asset('img/prestasi/chris.png') }}" alt="">
+                </div>
+                <div class="row">
+                    <p><strong>Christian Vieri</strong></p>
+                </div>
+            </div>
+        </div>
+        <div class="small-prestasi row pb-5">
+            <div class="col-12">
+                <div class="row mb-5">
+                    <div class="col-12 text-center">
+                        <h4><strong>Co-2<sup>nd</sup> Runner Up DUNNO 2021</strong></h4>
+                        <h4 class="prestasi-divisi"><strong>ITS Debating Society</strong></h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 d-flex justify-content-center">
+                        <div class="row">
+                            <img class="prestasi-card py-3" src="{{ asset('img/prestasi/oma.png') }}" alt="">
+                        </div>
+                    </div>
+                    <div class="col-6 d-flex justify-content-center">
+                        <div class="row ">
+                            <img class="prestasi-card py-3" src="{{ asset('img/prestasi/chris.png') }}" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="large-prestasi row d-flex justify-content-center align-items-center mb-4">
+            <div class="col-2">
+                <div class="row d-flex justify-content-end">
+                    <p><strong>Indrajit</strong></p>
+                </div>
+                <div class="row">
+                    <img class="prestasi-card" src="{{ asset('img/prestasi/jarjit.png') }}" alt="">
+                </div>
+                <div class="row">
+                    <img class="prestasi-circle" src="{{ asset('img/prestasi/circle.png') }}" alt="">
+                </div>
+            </div>
+            <div class="col-8" style="text-align: center">
+                <h4><strong>PURPOSE 2021 Quarterfinalist</strong></h4>
+                <h4 class="prestasi-divisi"><strong>ITS Debating Society</strong></h4>
+            </div>
+            <div class="col-2">
+                <div class="row">
+                    <img class="prestasi-circle" src="{{ asset('img/prestasi/circle.png') }}" alt="">
+                </div>
+                <div class="row">
+                    <img class="prestasi-card" src="{{ asset('img/prestasi/krisna.png') }}" alt="">
+                </div>
+                <div class="row">
+                    <p><strong>Putu Krisna</strong></p>
+                </div>
+            </div>
+        </div>
+        <div class="small-prestasi row pb-5">
+            <div class="col-12">
+                <div class="row mb-5">
+                    <div class="col-12 text-center">
+                        <h4><strong>PURPOSE 2021 Quarterfinalist</strong></h4>
+                        <h4 class="prestasi-divisi"><strong>ITS Debating Society</strong></h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 d-flex justify-content-center">
+                        <div class="row">
+                            <img class="prestasi-card py-3" src="{{ asset('img/prestasi/jarjit.png') }}" alt="">
+                        </div>
+                    </div>
+                    <div class="col-6 d-flex justify-content-center">
+                        <div class="row ">
+                            <img class="prestasi-card py-3" src="{{ asset('img/prestasi/krisna.png') }}" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="large-prestasi row d-flex justify-content-center align-items-center mb-4">
+            <div class="col-2">
+                <div class="row d-flex justify-content-end">
+                    <p><strong>Adelia</strong></p>
+                </div>
+                <div class="row">
+                    <img class="prestasi-card" src="{{ asset('img/prestasi/adelia.png') }}" alt="">
+                </div>
+                <div class="row">
+                    <img class="prestasi-circle" src="{{ asset('img/prestasi/circle.png') }}" alt="">
+                </div>
+            </div>
+            <div class="col-8" style="text-align: center">
+                <h4><strong>First Winner Fanart<br>Japan Corner Tanjousai 2<sup>nd</sup></strong></h4>
+                <h4 class="prestasi-divisi"><strong>IC Drawing Club</strong></h4>
+            </div>
+            <div class="col-2"></div>
+        </div>
+        <div class="small-prestasi row pb-5">
+            <div class="col-12">
+                <div class="row mb-5">
+                    <div class="col-12 text-center">
+                        <h4><strong>First Winner Fanart<br>Japan Corner Tanjousai 2<sup>nd</sup></strong></h4>
+                        <h4 class="prestasi-divisi"><strong>IC Drawing Club</strong></h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 d-flex justify-content-center">
+                        <div class="row">
+                            <img class="prestasi-card py-3" src="{{ asset('img/prestasi/adelia.png') }}" alt="">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
